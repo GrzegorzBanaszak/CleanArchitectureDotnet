1db3099b5086eaa5101501acc9311473aa5a621f
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2024-03-13 21:11:14 +0100

    Error handle examples

---
+++ BuberDinner.Api/Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace BuberDinner.Api.Middleware;
+
+public class ErrorHandlingMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ErrorHandlingMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception ex)
+        {
+
+            await HandleExeptionAsync(context, ex);
+        }
+    }
+
+
+    private static Task HandleExeptionAsync(HttpContext context, Exception ex)
+    {
+        var code = HttpStatusCode.InternalServerError;
+        var result = JsonSerializer.Serialize(new { error = "An error occured on request" });
+        context.Response.ContentType = "application/json";
+        context.Response.StatusCode = (int)code;
+        return context.Response.WriteAsync(result);
+    }
+}
--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,16 +1,27 @@
 using BuberDinner.Application;
 using BuberDinner.Infrastucture;
+using Microsoft.AspNetCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services.AddApplication().AddInfrastucture(builder.Configuration);
+    // builder.Services.AddControllers(opt => opt.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
-
+    // builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 
 {
+    //app.UseMiddleware<ErrorHandlingMiddleware>();
+    app.UseExceptionHandler("/error");
+    app.Map("/error", (HttpContext context) =>
+    {
+        Exception? exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+
+        return Results.Problem();
+    });
+
     app.UseHttpsRedirection();
     app.MapControllers();
 
